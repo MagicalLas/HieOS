eb5f58003f236bf739976606d31394843d2bec50
Wonho ha <haho013039@gmail.com>
2018-09-06 10:22:33 +0900

    색 지정

--- OS/OS/LasConsole.h
+++ OS/OS/LasConsole.h
@@ -2,4 +2,25 @@
 #define LASCONSOLE_H
 #include "windef.h"
 
+//콘솔의 색 지정
+enum ConsoleColor
+{
+	Black = 0,
+	Blue = 1,
+	Green = 2,
+	Cyan = 3,
+	Red = 4,
+	Magenta = 5,
+	Brown = 6,
+	LightGray = 7,
+	DarkGray = 8,
+	LightBlue = 9,
+	LightGreen = 10,
+	LightCyan = 11,
+	LightRed = 12,
+	LightMagenta = 13,
+	LightBrown = 14,
+	White = 15
+};
+
 #endif
