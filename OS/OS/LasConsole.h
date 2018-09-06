941b5670e6aa952df057a91fca2445a517564e38
Wonho ha <haho013039@gmail.com>
2018-09-06 10:30:47 +0900

    비디오 카드 주소 정의

--- OS/OS/LasConsole.h
+++ OS/OS/LasConsole.h
@@ -22,5 +22,14 @@ enum ConsoleColor
 	LightBrown = 14,
 	White = 15
 };
+/*
+http://www.osdever.net/FreeVGA/vga/vgareg.htm 여기보면 나와있을 것 같은데 모르겠다
+ioctl하기 위한 주소같다. 
+리눅스에서는 ioperm(set port input/output permissions)으로 
+ioperm(0x3b4, 0x3df - 0x3b4 + 1, 1)
+처럼 비디오를 켜거나 끈다. 그냥 정해져 있는 포트인듯
+*/
+#define VGA_COLOR_CRT_ADDRESS 0x3D4
+#define VGA_MONO_CRT_ADDRESS 0x3B4
 
 #endif
