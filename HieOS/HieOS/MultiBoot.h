2ab02b6f2d24c388ec0a44a5a853260de100335a
Wonho ha <haho013039@gmail.com>
2018-09-07 14:51:20 +0900

    일달 복붙인데 이게 그럽에서 주는 정보의 원형인듯

--- HieOS/HieOS/MultiBoot.h
+++ HieOS/HieOS/MultiBoot.h
@@ -55,3 +55,54 @@ struct AOUTSymbolTable
 	unsigned int addr;
 	unsigned int reserved;
 };
+
+struct multiboot_info
+{
+	/* Multiboot info version number */
+	uint32_t flags;
+
+	/* Available memory from BIOS */
+	uint32_t mem_lower;
+	uint32_t mem_upper;
+
+	/* "root" partition */
+	uint32_t boot_device;
+
+	/* Kernel command line */
+	char *cmdline;
+
+	/* Boot-Module list */
+	uint32_t mods_count;
+	Module *Modules;
+
+	union
+	{
+		AOUTSymbolTable AOUTTable;
+		ELFHeaderTable ELFTable;
+	} SymbolTables;
+
+	/* Memory Mapping buffer */
+	uint32_t mmap_length;
+	uint32_t mmap_addr;
+
+	/* Drive Info buffer */
+	uint32_t drives_length;
+	drive_info * drives_addr;
+
+	/* ROM configuration table */
+	ROMConfigurationTable *ConfigTable;
+
+	/* Boot Loader Name */
+	char* boot_loader_name;
+
+	/* APM table */
+	APMTable *APMTable;
+
+	/* Video */
+	VbeInfoBlock *vbe_control_info;
+	VbeModeInfo *vbe_mode_info;
+	uint16_t vbe_mode;
+	uint16_t vbe_interface_seg;
+	uint16_t vbe_interface_off;
+	uint16_t vbe_interface_len;
+};
