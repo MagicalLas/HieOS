6a3112f7045383c0ff6019e71f231040f52b4a24
Wonho ha <haho013039@gmail.com>
2018-09-07 10:32:24 +0900

    사이즈 정의

--- HieOS/HieOS/MultiBoot.h
+++ HieOS/HieOS/MultiBoot.h
@@ -12,3 +12,8 @@
 #define _Kernel_Stack 0x00400000
 #define _Free_Memory_Space_Adrr 0x00400000
 
+//설정해 놓은 것과 관련성 있음
+//책 참고
+#define ALIGN 0x400
+#define KERNEL_LOAD_ADDRESS 0x100000
+#define HEADER_ADRESS  KERNEL_LOAD_ADDRESS+ALIGN
