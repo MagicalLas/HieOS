ecc9490bb830ce48ad766d22a07e7f02572720b8
Wonho ha <haho013039@gmail.com>
2018-09-07 10:24:42 +0900

    emit 매크로 생성

--- HieOS/HieOS/MultiBoot.h
+++ HieOS/HieOS/MultiBoot.h
@@ -1,3 +1,10 @@
 #pragma once
 #include "stdint.h"
 #include "windef.h"
+
+//emit해주는 매크로 생성
+#define em(x)                            \
+		__asm _emit	    (x)	      & 0xff \
+		__asm _emit	    (x)	>> 8  & 0xff \
+		__asm _emit     (x) >> 16 & 0xff \
+		__asm _emit     (x) >> 24 & 0xff 
