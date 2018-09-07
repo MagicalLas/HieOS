62881427a32929cb6c2e96d8046af249d72cc75e
Wonho ha <haho013039@gmail.com>
2018-09-07 10:28:51 +0900

    커널 스택관련 크기설정

--- HieOS/HieOS/MultiBoot.h
+++ HieOS/HieOS/MultiBoot.h
@@ -8,3 +8,7 @@
 		__asm _emit	    (x)	>> 8  & 0xff \
 		__asm _emit     (x) >> 16 & 0xff \
 		__asm _emit     (x) >> 24 & 0xff 
+
+#define _Kernel_Stack 0x00400000
+#define _Free_Memory_Space_Adrr 0x00400000
+
