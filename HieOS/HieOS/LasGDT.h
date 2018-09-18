35e0d033d083942c3fb251dc0f48f3c0437bd918
Wonho ha <haho013039@gmail.com>
2018-09-19 08:25:46 +0900

    구조체 정의

--- HieOS/HieOS/LasGDT.h
+++ HieOS/HieOS/LasGDT.h
@@ -5,3 +5,14 @@
 #include "windef.h"
 #include "defines.h"
 
+//GDT구조체 정의
+struct GDT_descripter
+{
+	USHORT segment_limit;
+	USHORT base_low;
+	BYTE base_middle;
+	BYTE flag;
+	BYTE grand;
+	BYTE base_high;
+};
+
