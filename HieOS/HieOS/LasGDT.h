6db78fcebba80d6f75784ca07072abcd65c3521b
Wonho ha <haho013039@gmail.com>
2018-09-19 08:52:53 +0900

    자료형 변경.

--- HieOS/HieOS/LasGDT.h
+++ HieOS/HieOS/LasGDT.h
@@ -8,19 +8,19 @@
 //GDT구조체 정의
 struct GDT_descripter
 {
-	USHORT segment_limit;
-	USHORT base_low;
-	BYTE base_middle;
-	BYTE flag;
-	BYTE grand;
-	BYTE base_high;
+	uint16_t segment_limit;
+	uint16_t base_low;
+	uint8_t base_middle;
+	uint8_t flag;
+	uint8_t grand;
+	uint8_t base_high;
 };
 
 //레지스터에 넣을 구조체
 struct GDTR
 {
-	USHORT limit;
-	UINT base;
+	uint16_t limit;
+	uint8_t base;
 };
 
 //테이블 정의
