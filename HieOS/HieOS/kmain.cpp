15b93f4da911e6d91d81338a5f387613375ce3f9
Wonho ha <haho013039@gmail.com>
2018-09-18 12:36:25 +0900

    메인 코드 삭제

--- HieOS/HieOS/kmain.cpp
+++ HieOS/HieOS/kmain.cpp
@@ -33,25 +33,7 @@ _declspec(naked) void multiboot_entry(void)
 
 void kmain(unsigned long magic, unsigned long addr)
 {
-	unsigned int video_memory;
-	ushort* m_pVideoMemory = (unsigned short*)0xb0000;
 
-	char video_type = (*(unsigned short*)0x410 & 0x30);
+	for (;;);
 
-	if (video_type == 0x30) {
-		video_memory = VGA_MONO_CRT_ADDRESS;
-	}
-	else {
-		video_memory = VGA_COLOR_CRT_ADDRESS;
-	}
-
-	unsigned int m_ScreenHeight = 25;
-	unsigned int m_ScreenWidth = 80;
-
-	uint m_xPos = 0;
-	uint m_yPos = 0;
-
-	ConsoleColor m_Text = White;
-	ConsoleColor m_backGroundColor = Black;
-	ConsoleColor m_Color = (ConsoleColor)((m_backGroundColor << 4) | m_Text);
 }
