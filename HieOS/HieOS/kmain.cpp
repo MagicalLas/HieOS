c280e8e45eff3864e8e151e63a7ed4eb9b2df1be
Wonho ha <haho013039@gmail.com>
2018-09-18 12:36:10 +0900

    메인 어셈블리 수정

--- HieOS/HieOS/kmain.cpp
+++ HieOS/HieOS/kmain.cpp
@@ -6,20 +6,24 @@ _declspec(naked) void multiboot_entry(void)
 		align 4
 
 		multiboot_header:
-		em(MULTIBOOT_HEADER_MAGIC);
-		em(MULTIBOOT_HEADER_FLAGS);
-		em(CHECKSUM);
+		em(MULTIBOOT_HEADER_MAGIC); magic munber
+		em(MULTIBOOT_HEADER_FLAGS); flags
+		em(CHECKSUM); checksum
 		em(HEADER_ADRESS);
 		em(KERNEL_LOAD_ADDRESS);
 		em(00);
 		em(00);
 		em(HEADER_ADRESS + 0x20);
+
 		kernel_entry:
 		mov esp, _Kernel_Stack;
+
 		push 0;
 		popf
+
 		push ebx;
 		push eax;
+
 		call kmain;
 
 		h:
