b88fb5b64cc94f81011cdac3e2c7b8831698eb77
Wonho ha <haho013039@gmail.com>
2018-09-18 12:38:09 +0900

    필드 변수 정의

--- HieOS/HieOS/LasConsole.cpp
+++ HieOS/HieOS/LasConsole.cpp
@@ -1,3 +1,14 @@
 #include"LasConsole.h"
 namespace LasConsole {
+	static ConsoleColor _Color;
+	static ConsoleColor _Text;
+	static ConsoleColor _backGroundColor;
+
+	static uint _xPos;
+	static uint _yPos;
+
+	static ushort* _pVideoMemory; //Just a pointer to video memory
+	static unsigned int _ScreenHeight;
+	static unsigned int _ScreenWidth;
+	static unsigned short _VideoCardType;
 }
