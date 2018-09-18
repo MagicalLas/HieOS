e4fe9e4cd6769bdd6bc23f890b190d3db6f94c56
Wonho ha <haho013039@gmail.com>
2018-09-19 08:25:32 +0900

    메인문 삭제

--- HieOS/HieOS/kmain.cpp
+++ HieOS/HieOS/kmain.cpp
@@ -35,44 +35,6 @@ void kmain(unsigned long magic, unsigned long addr)
 {
 	LasConsole::init();
 	LasConsole::Clear();
-	LasConsole::move_cursor(1, 2);
-	LasConsole::print('W');
-	LasConsole::move_cursor(2, 2);
-	LasConsole::print('o');
-	LasConsole::move_cursor(3, 2);
-	LasConsole::print('n');
-	LasConsole::move_cursor(4, 2);
-	LasConsole::print('H');
-	LasConsole::move_cursor(5, 2);
-	LasConsole::print('o');
-	LasConsole::move_cursor(6, 2);
-	LasConsole::print('-');
-	LasConsole::move_cursor(7, 2);
-	LasConsole::print('L');
-	LasConsole::move_cursor(8, 2);
-	LasConsole::print('A');
-	LasConsole::move_cursor(9, 2);
-	LasConsole::print('S');
-
-
-	LasConsole::move_cursor(1, 3);
-	LasConsole::print('H');
-	LasConsole::move_cursor(2, 3);
-	LasConsole::print('i');
-	LasConsole::move_cursor(3, 3);
-	LasConsole::print('e');
-	LasConsole::move_cursor(4, 3);
-	LasConsole::print('O');
-	LasConsole::move_cursor(5, 3);
-	LasConsole::print('S');
-	LasConsole::move_cursor(6, 4);
-	LasConsole::print('M');
-	LasConsole::move_cursor(7, 4);
-	LasConsole::print('A');
-	LasConsole::move_cursor(8, 4);
-	LasConsole::print('D');
-	LasConsole::move_cursor(9, 4);
-	LasConsole::print('E');
 	for (;;);
 
 }
