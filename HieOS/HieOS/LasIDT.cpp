277b12e952d4f582ba705f1e89140c7aa855b6ca
Wonho ha <haho013039@gmail.com>
2018-09-27 23:44:26 +0900

    인터럽트 핸들러 설정 함수 구현

--- HieOS/HieOS/LasIDT.cpp
+++ HieOS/HieOS/LasIDT.cpp
@@ -5,3 +5,25 @@ static IDT_descripter	idt[256];
 IDT_descripter * get_interrupt_descripter(uint16_t index) {
 	return &idt[index];
 }
+bool install_interrupt_handler(uint16_t index, uint8_t flag, uint16_t selecter, I86_IRQ_HANDLER handler)
+{
+	if (!handler)
+		return false;
+
+	uint64_t		uiBase = (uint64_t)&(*handler);
+
+	if ((flag & 0x0500) == 0x0500) {
+		idt[index].selector = selecter;
+		idt[index].flag = flag;
+	}
+	else
+	{
+		idt[index].offset_low = uint16_t(uiBase & 0xffff);
+		idt[index].offset_high = uint16_t((uiBase >> 16) & 0xffff);
+		idt[index].reserved = 0;
+		idt[index].flag = flag;
+		idt[index].selector = selecter;
+	}
+
+	return	true;
+}
